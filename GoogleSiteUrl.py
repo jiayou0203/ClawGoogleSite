b5846ca6918df1fca21cfa6ad8d13470b68c9afb
Administrator <huoquan00022@163.com>
2022-07-28 10:42:41 +0800

    10.26

---
+++ GoogleSiteUrl.py
@@ -0,0 +1,27 @@
+import os
+import time
+import urllib
+from scrapy import Selector
+from selenium import webdriver
+
+proxy={"http":"xf123456789-zone-custom:123456:proxy.ipidea.io:2333"}
+
+class  GoogleSiteUrl:
+    def __init__(self):
+        self.browser=webdriver.Chrome('C:\Program Files\Google\Chrome\Application\chromedriver.exe')
+        self.key_world = input('Please input the content of the picture you want to grab >:')
+
+    def start_crawl(self):
+        self.browser.get('https://www.google.com/search?q=%s' %self.key_world)
+        self.browser.implicitly_wait(3)
+        xpath_urls='//div[@class='']/a"]'
+        urls_pre=self.browser.find_element_by_xpath(xpath_urls)
+        url=urls_pre[0].get_attribute("href")
+        time.sleep(3)
+        print(url)
+
+
+if __name__=='__main__':
+    google_url=GoogleSiteUrl()
+    google_url.start_crawl()
+
